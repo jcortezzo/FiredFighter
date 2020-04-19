7daec07a017b8ecef748dcf3b4356394199990a5
cortejon <cortejon@cs.washington.edu>
2020-04-19 01:32:31 -0700

    breakable

---
+++ FiredFighter/Assets/Resources/Scripts/Breakable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    public float health = 1f;
+    public int size;  // how much wood it produces
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Break();
+        }
+    }
+
+    private void Break()
+    {
+        // make wood
+    }
+
+    public void TakeHit(float damage, float hitstun)
+    {
+        health -= damage;
+        //this.hitstun = hitstun;
+    }
+}
