323a83805293a9376ed77b273abe8875431e79c6
Nguyen Duong <ducnguyenduchao@gmail.com>
2020-04-20 00:22:00 -0700

    add stove + gas template

---
+++ FiredFighter/Assets/Resources/Scripts/Gas.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gas : MonoBehaviour, IFlammable
+{
+    public void Burn()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public int FlameIncreaseNumber()
+    {
+        return 1;
+    }
+
+    public int SmokeNumber()
+    {
+        return 2;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FiredFighter/Assets/Resources/Scripts/Stove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stove : MonoBehaviour, IInteractable
+{
+
+    public Gas gasPref;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Interact(Player player)
+    {
+        
+    }
+
+    IEnumerator Smoke(IFlammable flammable)
+    {
+        int cycles = 5;
+        for (int i = 0; i < cycles; i++)
+        {
+            //CreateSmoke(flammable);
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
